375635b8ae29a3edda1067a33172f7d94e619ebc
lchscy <19839618021@163.com>
2026-01-11 19:17:07 +0800

    Site updated: 2026-01-11 19:17:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3357.html","posts/2700.html","posts/fd28.html","posts/224d.html","posts/629f.html","posts/3290.html","posts/df2a.html","posts/4963.html","posts/ab5b.html","posts/fb4c.html"];function toRandomPost(){
+var posts=["posts/3357.html","posts/fd28.html","posts/224d.html","posts/629f.html","posts/2700.html","posts/3290.html","posts/df2a.html","posts/4963.html","posts/fb4c.html","posts/ab5b.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
